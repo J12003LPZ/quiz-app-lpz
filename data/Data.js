db1f52a0541d83cd7c193cd8ee2ea913f2f754b0
J12003LPZ <104168530+J12003LPZ@users.noreply.github.com>
2022-11-19 11:55:50 -0500

    Update Data.js

--- data/Data.js
+++ data/Data.js
@@ -1,17 +1,34 @@
 const Data = [
   {
-    question: "Which is the biggest country in the world?",
-    choices: ["USA", "UK", "Russia", "China"],
-
+    question: "What's the world's largest country?",
+    choices: ["United States", "UK", "Russia", "China"],
     answer: "Russia",
   },
 
   {
-    question: "Who is the most preatty create on the Earth?",
-    choices: ["you", "ur mom", "you again dummy"],
-
-    answer: "you",
+    question: "Entomology is the science that studies",
+    choices: ["Behavior of human beings", "Insects", "The origin and history of technical and scientific terms","The formation of rocks"],
+    answer: "Insects",
   },
+  
+  {
+    question: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    choices: ["Asia", "Africa", "Europe", "Australia"],
+    answer: "Africa",
+  },
+  
+  {
+    question: "What is the largest continent in the world?",
+    choices: ["Asia", "America", "Europe", "Africa", "Australia"],
+    answer: "Asia",
+  },
+  
+  {
+    question: "When is the Independence Day of the United States?",
+    choices: ["July 1", "July 23", "July 5", "July 4"],
+    answer: "July 4",
+  },
+  
 ];
 
 export default Data;
